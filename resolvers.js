57244e797c6b33d47f49de005dded5b96564bbf6
hari <srsorg@srssoftwaresol.com>
2023-10-10 07:49:34 -0700

    first checkin

---
+++ resolvers.js
@@ -0,0 +1,24 @@
+import { getCompany } from './db/companies.js'
+import { getJob, getJobs, getJobsByCompany } from './db/jobs.js'
+import { GraphQLError } from 'graphql'
+
+export const resolvers = {
+  Query: {
+    company: (_root, { id }) => getCompany(id),
+    job: (_root, { id }) => getJob(id),
+    jobs: () => getJobs(),
+  },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
+  },
+}
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length)
+}
